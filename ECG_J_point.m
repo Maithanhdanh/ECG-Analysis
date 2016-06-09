929f3a7273b5f4f9967ac11a460b4c5814be4269
Maithanhdanh <mtd.maithanhdanh@gmail.com>
2016-06-09 21:14:00 +0700

    code detect J point

--- ECG_J_point.m
+++ ECG_J_point.m
@@ -60,40 +60,15 @@ left = find(diff([0 poss_reg])==1);
 right = find(diff([poss_reg 0])==-1);
 
 for i=1:length(left)
-    
-    %detect R peak
-    [R_value(i) R_loc(i)] = max( c(left(i):right(i)) );
-    R_loc(i) = R_loc(i)-1+left(i); 
-    
-    %detectQ peak
-    [Q_value(i) Q_loc(i)] = min( c(left(i):R_loc(i)) );
-    Q_loc(i) = Q_loc(i)-1+left(i); 
-
-    %detect S peak
-    [S_value(i) S_loc(i)] = min( c(left(i):right(i)) );
-    S_loc(i) = S_loc(i)-1+left(i); 
-    
-    %detect T peak
-    if i>1
-        [T_value(i-1) T_loc(i-1)] = max(c(S_loc(i-1):Q_loc(i)));
-        T_loc(i-1)=T_loc(i-1)+19+left(i-1);
-    end 
-    
     %detect J point
     J_loc(i)=right(i);          %J point is the end of QRS offset
     J_value(i)=c(J_loc(i));
 end
-    
-%% there is no selective wave %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-Q_loc=Q_loc(find(Q_loc~=0));
-R_loc=R_loc(find(R_loc~=0));
-S_loc=S_loc(find(S_loc~=0));
-
 %% plot figure
 figure
 plot (t(1:length(c)),c);
 hold on;
-plot(t(T_loc),T_value, 'o',t(J_loc),J_value, 'r^');
+plot(t(J_loc),J_value, 'r^');
 
 %% clear some veriable
 clear -regexp data x
